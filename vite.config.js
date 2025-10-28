acba250d4803464d4cf808c8fac17a8e6dbddd3c
sliemansalah <slieman2001@hotmail.com>
2025-10-28 10:57:55 +0300

    initalize project ..

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
+import { fileURLToPath, URL } from 'node:url';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/main.js'],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        vuetify({
+            autoImport: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./resources/js',
+                import.meta.url)),
+            '~': fileURLToPath(new URL('./resources',
+                import.meta.url)),
+        },
+    },
+    server: {
+        host: 'localhost',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+    },
+});
